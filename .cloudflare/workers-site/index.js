877f8ea24f61ea263070f74035b25de1200ff292
Lfrangt <lfrangt@icloud.com>
2025-05-05 21:22:44 +0800

    修复 Cloudflare Pages 配置问题

---
+++ .cloudflare/workers-site/index.js
@@ -0,0 +1,6 @@
+import { createEventHandler } from '@cloudflare/next-on-pages';
+
+export default createEventHandler({
+  // Optional: Enable streaming with server rendering
+  streamingEnabled: true,
+}); 
